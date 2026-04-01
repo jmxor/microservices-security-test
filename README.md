1b0d89c0c2ed36cfb2e36318af6bfce41b5f6191
jmxor <jamesmrippeth@gmail.com>
2026-04-01 06:27:13 +0100

    Fix port issues and incorrect reactive spring imports

--- README.md
+++ README.md
@@ -11,4 +11,7 @@ Add the following to the `/etc/hosts`
 ```bash
 127.0.0.1 api.local.test
 127.0.0.1 app.local.test
+127.0.0.1 keycloak.local.test
 ```
+
+TODO add keycloak testcontainer
--- demo-api-server/src/main/java/com/jmxor/demo_api_server/DemoApiConfig.java
+++ demo-api-server/src/main/java/com/jmxor/demo_api_server/DemoApiConfig.java
@@ -1,18 +1,16 @@
 package com.jmxor.demo_api_server;
 
-import java.beans.Customizer;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
